6c1442a5bbddc5fe2d2804b2093c742dd7bf098f
Shingo Yamada <uzuranoie@gmail.com>
2020-06-20 22:40:26 +0900

    Updated env.sh

--- src/server/middlewares/aws/s3-admin.js
+++ src/server/middlewares/aws/s3-admin.js
@@ -1,7 +1,7 @@
 import AWS from 'aws-sdk'
 import awsConfig from '@/config/aws-config'
 
-const AWS_S3_REGION = process.env.AWS_S3_REGION || awsConfig.s3.region
+const AWS_S3_REGION = process.env.AWS_S3_REGION || process.env.AWS_DEFAULT_REGION || awsConfig.s3.region
 AWS.config.update({region: AWS_S3_REGION});
 
 const s3 = new AWS.S3();
