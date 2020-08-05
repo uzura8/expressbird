15cfe4665aa1c12c258dca5d1069fb9a8bccd302
Shingo Yamada <uzuranoie@gmail.com>
2020-08-05 21:52:33 +0900

    Fixed variable

--- infra/modules/aws/iam/output.tf
+++ infra/modules/aws/iam/output.tf
@@ -1,8 +1,8 @@
 output "iam_role_fargate_task" {
-  value = aws_iam_role.fargate_task.arn
+  value = aws_iam_role.fargate_task
 }
 
 output "iam_role_fargate_task_execution" {
-  value = aws_iam_role.fargate_task_execution.arn
+  value = aws_iam_role.fargate_task_execution
 }
 
