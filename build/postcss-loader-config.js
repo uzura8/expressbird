294cee2f527daf6c6649829e169b62dec4bcdd80
Shingo Yamada <uzuranoie@gmail.com>
2020-09-10 22:04:01 +0900

    Fixed postcss-loader-config

--- build/postcss-loader-config.js
+++ build/postcss-loader-config.js
@@ -1,18 +1,14 @@
 module.exports = {
   loader: 'postcss-loader',// Setting for PostCSS
   options: {
-    plugins: () => [
-      // Enable Autoprefixer
-      // Add vendor prefix
-      require('autoprefixer')({
-        grid: true, // use CSS Grid Layout
-        flexbox: true,
-        //browsers: [
-        //  'last 2 versions',
-        //  'ie >= 11'
-        //]
-      })
-    ],
-    sourceMap: true
+    postcssOptions: {
+      plugins: [
+        require('autoprefixer')({
+          grid: true, // use CSS Grid Layout
+          flexbox: true,
+        })
+      ],
+    },
+    sourceMap: true,
   }
 }
